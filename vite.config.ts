94056376e5f2799329ea1baac164a2dc259e3258
WouterLab <panov135@mail.ru>
2024-01-28 23:03:04 +0300

    edit pckg.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import linaria from "@linaria/vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    dts(),
     linaria({
       include: ["**/*.{ts,tsx}"],
       babelOptions: {
