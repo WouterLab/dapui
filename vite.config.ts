ff8ad6446083ff65c872f662425ab663a54c41d4
WouterLab <panov135@mail.ru>
2024-01-28 21:08:03 +0300

    edit configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import linaria from "@linaria/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,19 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ponikit",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
 });
