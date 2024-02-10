f2840703fb8e05abd290a158a1466d12ef3ddb6a
WouterLab <panov135@mail.ru>
2024-02-10 15:23:41 +0300

    feat: updated default storybook theming

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Ponikit UI",
+  brandUrl: "https://t.me/boymep",
+  brandImage: "https://i.imgur.com/P4fFSB1.png",
+  brandTarget: "_self",
+});
